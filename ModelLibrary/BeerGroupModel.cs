3558815ff29836010a7d225db607d5df6e4871d5
caspermortensen <cmor21@student.aau.dk>
2023-11-14 14:48:36 +0100

    Added project files for each layer and connect

--- ModelLibrary/BeerGroupModel.cs
+++ ModelLibrary/BeerGroupModel.cs
@@ -2,11 +2,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace ModelLibrary
-
-public class BeerGroupModel
 {
-    public Guid BeerGroupID { get; set; }
-    public string GroupName { get; set; }
-    public List<BeerLoverModel> GroupMembers { get; set; }
+    public class BeerGroupModel
+    {
+        public Guid BeerGroupID { get; set; }
+        public string GroupName { get; set; }
+        public List<BeerLoverModel> GroupMembers { get; set; }
 
+    }
 }
--- ModelLibrary/RatingModel.cs
+++ ModelLibrary/RatingModel.cs
@@ -2,21 +2,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace ModelLibrary
-
-public class RatingModel
 {
-    public Guid RatingID { get; set; }
+    public class RatingModel
+    {
+        public Guid RatingID { get; set; }
 
-    [Column(TypeName = "decimal(6, 2)")]
-    public decimal RatingScore { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal RatingScore { get; set; }
 
-    public BeerModel Beer { get; set; }
+        public BeerModel Beer { get; set; }
 
-    public BeerLoverModel Taster { get; set; }
+        public BeerLoverModel Taster { get; set; }
 
-    public DateTime RatingDate { get; set; }
+        public DateTime RatingDate { get; set; }
 
-    public string? Review { get; set; }
+        public string? Review { get; set; }
 
 
+    }
 }
