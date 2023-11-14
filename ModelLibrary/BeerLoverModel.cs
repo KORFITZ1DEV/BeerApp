a904ab1ad709056e0f4d1283467055a207f9729f
caspermortensen <cmor21@student.aau.dk>
2023-11-14 11:38:24 +0100

    Model update Beer and BeerLover

--- ModelLibrary/BeerLoverModel.cs
+++ ModelLibrary/BeerLoverModel.cs
@@ -6,6 +6,6 @@ public class BeerLoverModel
     public Guid BeerLoverID { get; set; }
     public string BeerLoverName { get; set; }
     public string BeerLoverEmail { get; set; }
-    public List<BeerGroupModel> BeerGroups { get; set; }
-    public List<RatingModel> rating { get; set; }
+    public List<BeerGroupModel>? BeerGroups { get; set; }
+    public List<RatingModel>? rating { get; set; }
 }
